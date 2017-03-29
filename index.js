49ba4b335a1d328d80ea1574fcf6b03312c681a8
CharlesRouillard <rouillard.charles@hotmail.com>
2017-03-29 10:17:46 +0200

    add bramas func

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ client.on('message', msg => {
 });
 
 client.on('presenceUpdate', function(oldMember, newMember) {
-	console.log(newMember.presences)
+	console.log(newMember.presence)
 	if(newMember.user.username == 'bramas'){
 		newMember.sendMessage("Bonjour maitre, je suis le bot de Charles et David, que puis-je faire pour vous aujourd'hui ?");
 	}
