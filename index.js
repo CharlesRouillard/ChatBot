450dac0a1cc314f45052ff90b75cf262675b81b4
CharlesRouillard <rouillard.charles@hotmail.com>
2017-03-29 11:52:54 +0200

    add meteo

--- index.js
+++ index.js
@@ -35,19 +35,16 @@ function response(msg,isTag){
 		        url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + process.env.WEATHER_TOKEN,
 		        method: 'GET'
 	    	}).then(function(response){
-	    		if(response.data.cod == '404' || response.data.cod == '502'){
-	    			msg.reply('Ville inconnu ou incorrect');
-	    		}
-	    		else{
-		    		var temp = (response.data.main.temp-273.15);
-		    		temp = Math.round(temp *10)/10;
-		    		var tempMin = (response.data.main.temp_min-273.15);
-		    		tempMin = Math.round(tempMin *10)/10;
-		    		var tempMax = (response.data.main.temp_max-273.15);
-		    		tempMax = Math.round(tempMax *10)/10;
-		        	msg.reply("In " + response.data.name + " the current temperature is " + temp + "°C\nThe minimal temperature is " + tempMin + "°C and the maximal is " + tempMax + "°C\n");
-	    		}
-	    	}).catch(console.log);
+	    		var temp = (response.data.main.temp-273.15);
+	    		temp = Math.round(temp *10)/10;
+	    		var tempMin = (response.data.main.temp_min-273.15);
+	    		tempMin = Math.round(tempMin *10)/10;
+	    		var tempMax = (response.data.main.temp_max-273.15);
+	    		tempMax = Math.round(tempMax *10)/10;
+	        	msg.reply("In " + response.data.name + " the current temperature is " + temp + "°C\nThe minimal temperature is " + tempMin + "°C and the maximal is " + tempMax + "°C\n");
+	    	}).catch(function(fail){
+	    		msg.reply('Ville inconnu ou incorrect');
+	    	});
 	    }
 	    else{
 	    	msg.reply('Commande météo incorrect. Je comprend seulement : !meteo <city>\nJe comprend également : !blague');
