6c56776899328bdca7fa1c0a26a32a6c3c845ec4
CharlesRouillard <rouillard.charles@hotmail.com>
2017-03-29 10:01:52 +0200

    add mmbver

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on('message', msg => {
 });
 
 client.on('presenceUpdate', function(oldMember, newMember) {
-	console.log(oldMember.presence, '=>', newMember.presence);
+	console.log(newMember);
 });
 
 client.login(process.env.DISCORD_TOKEN);
